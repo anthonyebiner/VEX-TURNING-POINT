93300ce08d43670d3b53137c6623f32b96e0c188
crazycrabman <anthonyebiner@gmail.com>
2018-12-14 09:51:40 -0800

    removed one set of buttons

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -10,13 +10,7 @@ static lv_res_t btnm_action(lv_obj_t * btnm, const char *txt)
 
 void initGraphics() {
 	/*Create a button descriptor string array*/
-	static const char * btnm_map[] = {"1", "2", "3", "4", "\n", "5", "6", "7", "8",""};
-
-	/*Create a default button matrix*/
-	lv_obj_t * btnm1 = lv_btnm_create(lv_scr_act(), NULL);
-	lv_btnm_set_map(btnm1, btnm_map);
-	lv_btnm_set_action(btnm1, btnm_action);
-	lv_obj_set_size(btnm1, LV_HOR_RES, LV_VER_RES / 2);
+	static const char * btnm_map[] = {"Red Front 1", "Red Front 2", "Red Back 1", "Red Back 2", "\n", "Blue Front 1", "Blue Front 2", "Blue Back 1", "Blue Back 2",""};
 
 	/*Create a new style for the button matrix back ground*/
 	static lv_style_t style_bg;
@@ -44,9 +38,11 @@ void initGraphics() {
 	style_btn_pr.text.color = LV_COLOR_MAKE(0xbb, 0xd5, 0xf1);
 
 	/*Create a second button matrix with the new styles*/
-	lv_obj_t * btnm2 = lv_btnm_create(lv_scr_act(), btnm1);
+	lv_obj_t * btnm2 = lv_btnm_create(lv_scr_act(), NULL);
 	lv_btnm_set_style(btnm2, LV_BTNM_STYLE_BG, &style_bg);
 	lv_btnm_set_style(btnm2, LV_BTNM_STYLE_BTN_REL, &style_btn_rel);
 	lv_btnm_set_style(btnm2, LV_BTNM_STYLE_BTN_PR, &style_btn_pr);
-	lv_obj_align(btnm2, btnm1, LV_ALIGN_OUT_BOTTOM_MID, 0, 0);
+  lv_btnm_set_map(btnm2, btnm_map);
+  lv_btnm_set_action(btnm2, btnm_action);
+  lv_obj_set_size(btnm2, LV_HOR_RES, LV_VER_RES);
 }
